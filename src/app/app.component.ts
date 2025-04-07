0fd45cf730216aee8212b2814d26e613aba8fb8d
_kt_ismail <_kt_ismail@kt-ismails-Air.bbrouter>
2025-04-07 08:38:34 +0530

    basic lavel reponsive added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,11 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LeftPaneComponent } from './components/left-pane/left-pane.component';
-import { MiddlePaneComponent } from './components/middle-pane/middle-pane.component';
-import { RightPaneComponent } from './components/right-pane/right-pane.component';
-import { RightDrawerComponent } from './components/right-drawer/right-drawer.component';
-import { FieldGroupService } from './service/field-group.service';
-
 @Component({
   selector: 'app-root',
-  imports: [
-    LeftPaneComponent,
-    MiddlePaneComponent,
-    RightPaneComponent,
-    RightDrawerComponent
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-
-  private fgService = inject(FieldGroupService)
-  editMode = false;
-
-
+export class AppComponent {
   title = 'zuper-test';
-
-
-  ngOnInit(): void {
-      this.fgService.clickedForEdit$.subscribe(res => {
-        if(res){
-          console.log(res);
-          
-          this.editMode = true;
-        }else{
-          this.editMode = false
-        }
-      })
-  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'landing', pathMatch: 'full',
+    },
+    {
+        path: 'landing', component: LandingComponent
+    },
+    {
+        path: 'home', component: HomeComponent
+    },
+    {
+        path: '**', component: PageNotFoundComponent
+    }
+];
